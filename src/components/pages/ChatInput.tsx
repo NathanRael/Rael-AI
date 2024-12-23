1056dd3bc0426ff7bc8e3063526c5927b77fe18a
NathanRael <ralaivoavy.natanael@gmail.com>
2024-12-23 21:50:27 +0300

    refactor(ChatInput) : place the message submission logic in onSucess mutation

--- src/components/pages/ChatInput.tsx
+++ src/components/pages/ChatInput.tsx
@@ -4,7 +4,7 @@ import useSmartTextarea from "@/hooks/useSmartTextarea.ts";
 import {ArrowUp, StopCircle} from "lucide-react";
 import {Icon, Textarea, useToast} from "rael-ui";
 import {useNavigate, useParams, useSearchParams} from "react-router-dom";
-import {useEffect, useMemo, useState} from "react";
+import {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {newConversation} from "@/api/conversationsApi.ts";
 import useFetchConversations from "@/hooks/useFetchConversations.ts";
@@ -18,107 +18,105 @@ import useFile from "@/hooks/useFile.ts";
 import {useModelStore} from "@/store/modelStore.ts";
 
 const ChatInput = () => {
-    const [file, setFile] = useState<File | null>(null)
-    const {toast, renderToastContainer} = useToast();
-    const {chatId} = useParams();
-    const {submitting, handleSubmitMessage} = useMessageContext();
-    const [message, setMessage] = useState("");
-    const {scrollToBottom} = useScroll();
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
-    const [success, setSuccess] = useState(false)
+    const [file, setFile] = useState<File | null>(null);
+    const inputRef = useRef<HTMLTextAreaElement>(null);
+    const [message, setMessage] = useState('');
+
+    const { toast, renderToastContainer } = useToast();
+    const { chatId } = useParams();
     const [searchParams] = useSearchParams();
-    const user = useUserStore(state => state.user);
-    const formatedModels = useModelStore(state => state.formatedModels)
-    const visionModels = useMemo(() => formatedModels.filter(model => model.capability === 'vision'), [formatedModels]);
-    const models = useModelStore(state => state.models);
-    const updateSelectedModel = useModelStore(state => state.updateSelectedModel)
-    const selectedModel = useModelStore(state => state.selectedModel)
+    const navigate = useNavigate();
 
-    const canSubmit: boolean = useMemo(() => !submitting && message.trim() !== '', [submitting, message])
-    const chatbotTypeIdInParams = useMemo(() => searchParams.get('chatType'), [searchParams]);
+    const { submitting, handleSubmitMessage } = useMessageContext();
+    const { scrollToBottom } = useScroll();
+    const queryClient = useQueryClient();
 
+    const user = useUserStore((state) => state.user);
+    const models = useModelStore((state) => state.models);
+    const formatedModels = useModelStore((state) => state.formatedModels);
+    const updateSelectedModel = useModelStore((state) => state.updateSelectedModel);
+
+    const { uploadedImage, setUploadedImage, handleFileUpload } = useFile(file);
     const storage = useLocalStorage();
 
-    const {data: conversations, isLoading: isConversationLoading} = useFetchConversations({})
-    const {mutateAsync: newConversationMutation} = useMutation({
+    const { data: conversations, isLoading: isConversationLoading } = useFetchConversations({});
+    const { mutateAsync: newConversationMutation } = useMutation({
         mutationFn: newConversation,
         onSuccess: () => {
-            queryClient.invalidateQueries([queryKeys.conversationList])
-            setSuccess(true)
-        }
-    })
-    const {handleKeyPress} = useSmartTextarea({
-        onEnter: () => {
-            setMessage("");
-            if (!canSubmit)
-                return
-            handleSubmit()
-            scrollToBottom(document.body.scrollHeight);
+            queryClient.invalidateQueries([queryKeys.conversationList]);
+            
+            if (!conversations || conversations.length === 0) return;
+
+            const newConversationId = conversations[0].id;
+            const userInput = fetchFromStorage('userInput');
+            const uploadedFileId = fetchFromStorage('fileId');
+
+            navigate(`/chat/${newConversationId}`);
+            handleSubmitMessage(userInput, newConversationId, () => setMessage(''), chatbotTypeIdInParams!, uploadedFileId);
         },
-        value: message
     });
 
-    const {uploadedImage, setUploadedImage, handleFileUpload} = useFile(file);
+    const canSubmit = useMemo(() => !submitting && message.trim() !== '', [submitting, message]);
+    const chatbotTypeIdInParams = useMemo(() => searchParams.get('chatType'), [searchParams]);
+    const visionModels = useMemo(() => formatedModels.filter((model) => model.capability === 'vision'), [formatedModels]);
 
-    const resetImageContent = () => {
-        setUploadedImage('')
-        setFile(null)
-    }
+    const resetImageContent = useCallback(() => {
+        setUploadedImage('');
+        setFile(null);
+    }, [setUploadedImage, setFile]);
 
+    const fetchFromStorage = (key: string) => storage.getItem(key) || '';
 
     const handleSubmit = async () => {
-
-        if (file) {
-            const visionModel = models.filter(m => visionModels[0].name.split(':')[0].includes(m.split(':')[0]))[0]
-            updateSelectedModel(visionModel)
-        }
-
-
-        const uploadedFile = await handleFileUpload(file);
-
-
-        // Handle the submit when user already started a conversation
-        if (chatId && !isConversationLoading) {
-            const chatbotTypeId = conversations!.find(conversation => conversation.id === chatId)!.chatbot_type_id
-            resetImageContent()
-            await handleSubmitMessage(message, chatId, () => setMessage(''), chatbotTypeId, uploadedFile?.id)
-            return
-        }
-
-        storage.setItem('userInput', message)
-        storage.setItem('fileId', uploadedFile?.id || '')
-        setMessage('')
-        resetImageContent()
-
-        setSuccess(false)
         try {
-            const userInput = storage.getItem('userInput')
-            const generatedTitle: string = await generateMessage({prompt: `Give me a suitable title for this message : '${userInput}'.Don't be verbose.Just give the response without commentary`})
+            let uploadedFileId = '';
+            
+            if (file) {
+                const visionModel = models.find((m) =>
+                    visionModels[0].name.split(':')[0].includes(m.split(':')[0])
+                );
+                updateSelectedModel(visionModel!);
+                const uploadedFile = await handleFileUpload(file);
+                uploadedFileId = uploadedFile?.id || '';
+            }
+
+            if (chatId && !isConversationLoading) {
+                const chatbotTypeId = conversations!.find((conv) => conv.id === chatId)!.chatbot_type_id;
+                resetImageContent();
+                await handleSubmitMessage(message, chatId, () => setMessage(''), chatbotTypeId, uploadedFileId);
+                return;
+            }
+
+            storage.setItem('userInput', message);
+            storage.setItem('fileId', uploadedFileId);
+            setMessage('');
+            resetImageContent();
+
+            const generatedTitle = await generateMessage({
+                prompt: `Give me a suitable title for this message: '${message}'. Don't be verbose. Just the title.`,
+            });
+
             await newConversationMutation({
                 user_id: user.id,
                 title: generatedTitle,
-                chatbot_type_id: chatbotTypeIdInParams || ''
-
-            })
-        } catch (e) {
-            console.error(e)
+                chatbot_type_id: chatbotTypeIdInParams || '',
+            });
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-
-    // Redirecting the user to the  created conversation
-    useEffect(() => {
-        if (conversations && !isConversationLoading && success) {
-            const newConversationId = conversations[0].id
-            const userInput = JSON.parse(localStorage.getItem('userInput') || '')
-            const uploadedFileId = JSON.parse(localStorage.getItem('fileId') || '')
-
-            navigate(`/chat/${newConversationId}`);
-
-            handleSubmitMessage(userInput, newConversationId, () => setMessage(''), chatbotTypeIdInParams!, uploadedFileId)
-        }
-    }, [conversations]);
+    const { handleKeyPress } = useSmartTextarea({
+        onEnter: () => {
+            if (canSubmit) {
+                setMessage('');
+                handleSubmit();
+                scrollToBottom(document.body.scrollHeight);
+            }
+        },
+        value: message,
+    });
+    
 
 
     return (
@@ -129,6 +127,7 @@ const ChatInput = () => {
                     resetImageContent()
                 }}/>}
             <Textarea
+                ref={inputRef}
                 size={'lg'}
                 value={message}
                 variant={'fill'}
