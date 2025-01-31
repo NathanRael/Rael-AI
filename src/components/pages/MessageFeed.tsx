4cfcfff415fa480eac8df7b75146938b57f533dd
nathanrael <ralaivoavy.natanael@gmail.com>
2025-01-31 18:09:35 +0300

    feat(message) : add chain of thought component for message

--- src/components/pages/MessageFeed.tsx
+++ src/components/pages/MessageFeed.tsx
@@ -2,7 +2,7 @@
 import {copyToClipboard} from "@/utils/helpers.ts";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import {Code2, Copy} from "lucide-react";
+import {ChevronDown, ChevronUp, Code2, Copy} from "lucide-react";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 import {dracula} from "react-syntax-highlighter/dist/esm/styles/prism";
 import useWindowSize from "@/hooks/useWindowSize.ts";
@@ -82,7 +82,8 @@ const UserImageView = ({image, loading, error, reFetch}: {
         return <ErrorUI error={error} onRetry={reFetch}/>
 
     if (loading)
-        return <div className={'max-md:size-[128px] size-[210px] object-cover rounded-xl animate-pulse dark:bg-white/10 bg-black/20'}/>
+        return <div
+            className={'max-md:size-[128px] size-[210px] object-cover rounded-xl animate-pulse dark:bg-white/10 bg-black/20'}/>
 
     return (
         <img src={image} alt={'user Image'} className={'max-md:size-[128px] size-[210px]  object-cover rounded-xl'}/>
@@ -91,6 +92,12 @@ const UserImageView = ({image, loading, error, reFetch}: {
 
 const BotMessage = ({content, handleCopy}: { content: string, handleCopy: (text: string) => Promise<void> }) => {
     const {width} = useWindowSize();
+    const messageRegex = /^<think>(.*?)<\/think>(.*)$/s;
+    const match = messageRegex.exec(content);
+
+    const chainOfThought = match ? match[1].trim() : '';
+    const finalResponse = match ? match[2].trim() : '';
+
 
     const generateWidth = () => {
         if (width >= 668)
@@ -107,16 +114,13 @@ const BotMessage = ({content, handleCopy}: { content: string, handleCopy: (text:
     return (
         <div className={'prose prose-style relative'}>
             <CopyIcon className={'absolute -bottom-6 py-1 -left-2 '} onClick={() => handleCopy(content)}/>
-            <ReactMarkdown className={'text-wrap w-full'} children={content} remarkPlugins={[remarkGfm]}
+            <ChainOfThoughtMessage message={chainOfThought}/>
+            <ReactMarkdown className={'text-wrap w-full'} children={finalResponse} remarkPlugins={[remarkGfm]}
                            components={{
-                               code(props : any) {
+                               code(props: any) {
                                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                    const {children, className, node, ...rest} = props
                                    const match = /language-(\w+)/.exec(className || '')
-                                   // const chainOfThought = /<think>(.)+/;
-
-                                   console.log(content)
-                                   // console.log(chainOfThought.test(content))
                                    const codeContent = String(children).replace(/\n$/, '');
                                    return match ? (
                                        <div className={'relative'}>
@@ -176,6 +180,31 @@ const CopyIcon = ({
             {!forCode && <span className={'text-[12px]'}>{copied ? 'copied' : 'copy'}</span>}
             {forCode && <span className={'text-[12px]'}>{copied ? 'code copied' : 'copy code'}</span>}
         </Icon>
+    )
+}
+
+
+const ChainOfThoughtMessage = ({message}: { message: string }) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    if (!message)
+        return
+
+    return (
+        <section className={'flex flex-col items-start  gap-2 '}>
+            <div
+                onClick={() => setIsCollapsed(!isCollapsed)}
+                className={'flex items-center cursor-pointer justify-center gap-2 bg-neutral-light-80 dark:bg-neutral-dark-80 px-3 py-2 rounded-xl'}>
+                <span>Chain of thought</span>
+                {isCollapsed ? <ChevronUp size={20}/> : <ChevronDown size={20}/>}
+            </div>
+            {
+                isCollapsed && (
+                    <div className={' border-0 border-l-2 border-l-neutral-light-80 dark:border-l-neutral-dark-80  p-3  '}>
+                        {message}
+                    </div>)
+            }
+        </section>
     )
 }
 
